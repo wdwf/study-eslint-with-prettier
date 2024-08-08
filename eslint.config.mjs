b1a6b4397a9153ab4f51c75b01c72af13ce75102
wdwf <w_dwf@hotmail.com>
2024-08-08 17:52:30 -0300

    chore: add regras ao eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,38 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
 
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
-];
+	{
+		rules: {
+			'no-unused-vars': 'error',
+			'no-undef': 'error',
+			'indent': [
+				'error',
+				'tab'
+			],
+			'quotes': [
+				'error',
+				'single'
+			],
+			'semi': [
+				'error',
+				'never'
+			],
+			'linebreak-style': [
+				'error',
+				'unix'
+			]
+		}, 
+	},
+	{files: ['**/*.js'], languageOptions: {sourceType: 'commonjs'}},
+	{languageOptions: { globals: globals.node }},
+	pluginJs.configs.recommended,
+]
+
+/*
+  nível de regra
+  "off" or 0 - desabilita a regra
+  "warn" or 1 - deixa como aviso (não tem efeito no código)
+  "error" or 2 - deixa como erro (tem efeito no código)
+*/
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-const server = require("express")();
+const server = require('express')()
 
+const ALWAYS = 'valor'
+
+console.log(ALWAYS)
+
+// eslint-disable-next-line no-unused-vars
 server.get('/', function(req, res){})
 
-server.listen(3000)
+server.listen(3000) 
